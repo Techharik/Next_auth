4b2782e2c1f28883d6a86288ceeeba67a6df83f3
Techharik <khariprasath30@gmail.com>
2023-08-28 17:39:16 +0530

    s

---
+++ src/app/comp/first.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function First() {
+  return (
+    <div>first</div>
+  )
+}
+
+export default First
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import First from '../comp/first';
+
+
 
 function Profile() {
   return (
+    <>
     <div>Profile</div>
-  )
+    <First />
+    </>
+    
+    )
 }
 
 export default Profile;
